4ce93c80c347f05982904904013ebc56ffaa0eb5
NKVharshvardhan <73694587+NKVharshvardhan@users.noreply.github.com>
2020-12-20 20:35:12 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,480 @@
+createRecord("Q", {OPEAN:'PROJECT'}, function(record) {
+  
+});
+setScreen("screen1");
+hideElement("image14");
+hideElement("image21");
+onEvent("button1", "click", function( ) {
+  var x = getText("text_input1");
+  if (x == "ys jagan mohan reddy took charge as CM of AP") {
+      setText("label2", "may 30 2019");
+    } else {
+      setText("label2", "please enter the corret question");
+    }
+  var Q = getText("text_input1");
+  if (Q ==  "what is the name of Jagan Mohan Reddy Wife" ) {
+      setText("label2", "YS barathi");
+    }
+  var dsdsds = getText("text_input1");
+  if (dsdsds ==  "" ) {
+      setText("label2", "Please Type a Questaion or click Help");
+    }
+  var f = getText("text_input1");
+  if (f ==  "what is the Full Name of Y S Jagan Mohan Reddy" ) {
+      setText("label2", "Yeduguri Sandinti Jagan Mohan Reddy");
+    }
+  var j = getText("text_input1");
+  if (j ==  "what is the full name of ysrcp Party" ) {
+    setText("label2", "Yuvajana Sramika Rythu Congress Party");
+  }
+  var n = getText("text_input1");
+  if (n ==  "where did jagan mohan reddy born" ) {
+    setText("label2", "Jagan Mohan Reddy was born on 21 december 1972 in kadapa district of andra predesh");
+  }
+  var h = getText("text_input1");
+  if (h ==  "what are the names of ys jagan daughters" ) {
+    setText("label2", "harashareddy  ,varshareddy");
+  }
+  var b = getText("text_input1");
+  if (b ==  "what is Jagan Mohan Reddy sister name" ) {
+    setText("label2", "Y S sharmila");
+  }
+  var THHFHFGH = getText("text_input1");
+  if (THHFHFGH ==  "where did Jagan Mohan Reddy father died" ) {
+    setText("label2", "nallamala hills,ANDHRAPRADESH 2 September 2009");
+  }
+  var s = getText("text_input1");
+  if (s ==  "what is Jagan Mohan Reddy qualification" ) {
+    setText("label2", "graduate");
+  }
+  var hh = getText("text_input1");
+  if (hh ==  "what is Height and weight of Jagan Mohan Reddy" ) {
+    setText("label2", "Height 6.1 weight 70 KG(approx)");
+  }
+  var p = getText("text_input1");
+  if (p ==  "Jagan Mohan Reddy religion was" ) {
+    setText("label2", "christian protestant");
+  }
+  var FD = getText("text_input1");
+  if (FD ==  "what is the date of birth of Jagan Mohan Reddy" ) {
+    setText("label2", "21 December 1972");
+  }
+  var dd = getText("text_input1");
+  if (dd ==  "What is the Place of Birth of y s Jagan" ) {
+    setText("label2", "  A. Jammalamadugu, District Kadapa");
+  }
+  var jj = getText("text_input1");
+  if (jj ==  "What is the Profission of Y S Jagan Before CM" ) {
+    setText("label2", " Agriculturist, Industrialist");
+  }
+  var gg = getText("text_input1");
+  if (gg ==  "What is the Premanant Address of YS Jagan" ) {
+    setText("label2", "3-9-77  Pulivendula, YSR District – 516390, Andhra Pradesh");FDDGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTRWQAZXFDFGHJL;L,MCVXZ
+  }
+  var mm = getText("text_input1");
+  if (mm ==  "Leader of opposition in the Andhra Pradesh Legislative Assembly" ) {
+    setText("label2", " 19 June 2014 to 29 May 2019");
+  }
+  var aa = getText("text_input1");
+  if (aa ==  "Other political affiliations" ) {
+    setText("label2", "Indian National Congress till 2011");
+  }
+  var AA = getText("text_input1");
+  if (AA ==  "First Political Entry of YS Jagan" ) {
+    setText("label2", "  By campaigning for Indian National Congress in the 2004 elections in Kadapa District, Andhra Pradesh");
+  }
+  var MN = getText("text_input1");
+  if (MN ==  " When he Become MP" ) {
+    setText("label2", "In the 2009 elections he was elected as Member of Parliament from Kadapa constituency as a member of the Indian National Congress");
+  }
+  var EEAA = getText("text_input1");
+  if (EEAA ==  " When YS Jagan Started Odarpu yatra" ) {
+    setText("label2", "In 2011");
+  }
+  var GHH = getText("text_input1");
+  if (GHH ==  "When YS Jagan Resigned INC Party" ) {
+    setText("label2", "On November 29, 2010");
+  }
+  var NN = getText("text_input1");
+  if (NN ==  " When YSRCP Party Announced" ) {
+    setText("label2", "In March 2011, he announced at Jagganapeta of East Godavari district");
+  }
+  var AAG = getText("text_input1");
+  if (AAG ==  "When YSRCP Participates in Electronics" ) {
+    setText("label2", "2014, Wins 64 of 175 seats");
+  }
+  var FF = getText("text_input1");
+  if (FF ==  "When PrajaSankalpaYatra - Padayatra Started" ) {
+    setText("label2", "PrajaSankalpaYatra on 6 November 2017");
+  }
+  var GHGHGH = getText("text_input1");
+  if (GHGHGH ==  " How many days and kilometres we walked in Padayatra" ) {
+    setText("label2", "Across 125 Assembly segments in 13 districts of the state in 430 days, 3648 Kilometres");
+  }
+  var SFD = getText("text_input1");
+  if (SFD ==  "When YSRCP padayatrs ends" ) {
+    setText("label2", "9 January 2019");
+  }
+  var SA = getText("text_input1");
+  if (SA ==  "When Knife attacked on YS Jagan, and Where" ) {
+    setText("label2", "Attacked with a cock fighting knife in the VIP Lounge of Visakhapatnam Airport on 25 October 2018");
+  }
+  var as = getText("text_input1");
+  if (as ==  "How many Seats Grabbed YSRCP in 2019 Elections" ) {
+    setText("label2", "National and State election held in April–May 2019, YSR Congress Party swept the polls and won 151 of the total 175 assembly seats and 22 of the 25 Lok Sabha seats in AP. He took oath as Chief Minister on 30 May 2019");
+  }
+  var bn = getText("text_input1");
+  if (bn ==  "What are the Business of YS Jagan" ) {
+    setText("label2", "Reddy founded the Telugu language daily newspaper Sakshi and the television channel Sakshi TV. He is the chief promoter of Bharathi Cements");
+  }
+  var cf = getText("text_input1");
+  if (cf ==  "What are YSRCP Navaratnalu" ) {
+    setText("label2", "1.Rythu Bharosa, 2.Fee Reimbursement, 3.Arogyasri, 4.Jalayagnam, 5.Ban On Alcohol, 6.Amma Vodi, 7.YSR Asara, 8.YSR Cheyutha,9.Pedhalandariki Illu");
+  }
+  var kkk = getText("text_input1");
+  if (kkk ==  "When was Sakshi news paper started publishing" ) {
+    setText("label2", 2008);
+  }
+  var jhjh = getText("text_input1");
+  var g = getText("text_input1");
+  if (g ==  "When and where Y S Jagan Started HUNGERS STRIKE" ) {
+    setText("label2", "Reddy started a hunger strike opposing the United Progressive Alliance's decision to endorse the creation of a separate Telangana state");
+  }
+  var nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn = getText("text_input1");
+  if (nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn ==  "in which place ys Jagan Mohan Reddy Temple constructed" ) {
+    setText("label2", "the Paschim Godavari district");
+  }
+  var qweqjjjjjjwe = getText("text_input1");
+  if (qweqjjjjjjwe ==  "Jagan Mohan Reddy favourite hero" ) {
+    setText("label2", "Nandamuri Balakrishna");
+  }
+  var fdfd = getText("text_input1");
+  if (fdfd == "what are the costumes jagan garu were regularly" ) {
+    setText("label2", "white colour Shirt,light brown colour Trouser");
+  }
+  var lq = getText("text_input1");
+  if (lq == "Who is the Third best CM in India" ) {
+    setText("label2", "YS jagan mohan reddy");
+  }FF
+  var az = getText("text_input1");
+  var jaganschool = getText("text_input1");
+  if (jaganschool == "jagan schooling") {
+    setText("label2", "Hyderabad Public school, Nizam College, Hyderabad");
+  }
+  var whatisjaganfavouraite = getText("text_input1");
+  if (whatisjaganfavouraite == "what is jagan favorite food") {
+    setText("label2", "rayalaseema food");
+  }
+  var FGFDGHGHFDGHFDG = getText("text_input1");
+  if (FGFDGHGHFDGHFDG == "JAGAN FAMILY IMAGES") {
+    showElement("image19");
+    showElement("image18");
+    setText("label2", "");
+  } else {
+    hideElement("image19");
+    hideElement("image18");
+  }
+  var fgfgngdsasxvcz = getText("text_input1");
+  if (fgfgngdsasxvcz == "jagan marrage date") {
+    setText("label2", "26 august 1996");
+  }
+  var gjktjhzryjhfgyxd3dbnhbhbkj = getText("text_input1");
+  if (gjktjhzryjhfgyxd3dbnhbhbkj == "tell me about Jagan father Y S Rajasekhara Reddy") {
+    setText("label2", "Rajasekhar Reddy was two time chief minister of India in Andhra Pradesh serving from 2004 to 2009");
+  }
+  var gjktjhzryjhfgyxd3dbJnhbhbkj = getText("text_input1");
+  if (gjktjhzryjhfgyxd3dbJnhbhbkj == "who created this app") {
+    setText("label2", "N.K.V HARSHAVARDHAN nkvharshvardhan@gmail.com");
+    showElement("image15");
+    showElement("image14");
+    onEvent("image14", "click", function( ) {
+      showElement("image21");
+      createRecord("LIKED", {CCC:'LIKED'}, function(record) {
+        
+      });
+    });
+  } else {
+    hideElement("image15");
+    hideElement("image21");
+    hideElement("image14");
+  }
+});
+onEvent("label8", "click", function( ) {
+  setScreen("screen2");
+});
+hideElement("image21");
+hideElement("image14");
+hideElement("image19");
+hideElement("image18");
+onEvent("button1111", "click", function( ) {
+  setText("text_input1", "who created this app");
+  setScreen("screen1");
+});
+onEvent("button32", "click", function( ) {
+  setText("text_input1", "jagan schooling");
+  setScreen("screen1");
+});
+hideElement("image15");
+onEvent("label7", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("button61", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("label6", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("label1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button47", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("label1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen2");
+  setNumber("text_input1", " ");
+  setText("text_input1", "");
+});
+onEvent("button36", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button14", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("label3", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("label4", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("label5", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button43", "click", function( ) {
+  setText("text_input1", "what is jagan favorite food");
+  setScreen("screen1");
+});
+onEvent("button46", "click", function( ) {
+  setText("text_input1", "jagan marrage date");
+  setScreen("screen1");
+});
+onEvent("button30", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "Who is the Third best CM in India");
+});
+onEvent("button38", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "JAGAN FAMILY IMAGES");
+});
+onEvent("button1111", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "who created this app");
+});
+onEvent("button31", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "what are the costumes jagan garu were regularly");
+});
+onEvent("button45", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "Jagan Mohan Reddy favourite hero");
+});
+onEvent("button29", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "in which place ys Jagan Mohan Reddy Temple constructed");
+});
+onEvent("button8", "click", function( ) {
+  setText("text_input1", "tell me about Jagan father Y S Rajasekhara Reddy");
+  setScreen("screen1");
+});
+onEvent("button19", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "When was Sakshi news paper started publishing");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "What is the Marriage Anniversary of Y S Jagan");
+});
+onEvent("button18", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "When and where Y S Jagan Started HUNGERS STRIKE");
+});
+onEvent("button42", "click", function( ) {
+  setScreen("screen1");
+  setText("text_input1", "What are YSRCP Navaratnalu");
+});
+onEvent("button41", "click", function( ) {
+  setText("text_input1", "What are the Business of YS Jagan");
+  setScreen("screen1");
+});
+onEvent("button40", "click", function( ) {
+  setText("text_input1", "How many Seats Grabbed YSRCP in 2019 Elections");
+  setScreen("screen1");
+});
+onEvent("button39", "click", function( ) {
+  setText("text_input1", "When Knife attacked on YS Jagan, and Where");
+  setScreen("screen1");
+});
+onEvent("button37", "click", function( ) {
+  setText("text_input1", "When YSRCP padayatrs ends");
+  setScreen("screen1");
+});
+onEvent("button35", "click", function( ) {
+  setText("text_input1", " How many days and kilometres we walked in Padayatra");
+  setScreen("screen1");
+});
+onEvent("button34", "click", function( ) {
+  setText("text_input1", "When PrajaSankalpaYatra - Padayatra Started");
+  setScreen("screen1");
+});
+onEvent("button26", "click", function( ) {
+  setText("text_input1", " When he Become MP");
+  setScreen("screen1");
+});
+onEvent("button27", "click", function( ) {
+  setText("text_input1", " When YS Jagan Started Odarpu yatra");
+  setScreen("screen1");
+});
+onEvent("button28", "click", function( ) {
+  setText("text_input1", "When YS Jagan Resigned INC Party");
+  setScreen("screen1");
+});
+onEvent("button24", "click", function( ) {
+  setText("text_input1", " When YSRCP Party Announced");
+  setScreen("screen1");
+});
+onEvent("button44", "click", function( ) {
+  setText("text_input1", "When YSRCP Participates in Electronics");
+  setScreen("screen1");
+});
+onEvent("button24", "click", function( ) {
+  setText("text_input1", "First Political Entry of YS Jagan");
+  setScreen("screen1");
+});
+onEvent("button23", "click", function( ) {
+  setText("text_input1", "Other political affiliations");
+  setScreen("screen1");
+});
+onEvent("button21", "click", function( ) {
+  setText("text_input1", "What is the Premanant Address of YS Jagan");
+  setScreen("screen1");
+});
+onEvent("button22", "click", function( ) {
+  setText("text_input1", "Leader of opposition in the Andhra Pradesh Legislative Assembly");
+  setScreen("screen1");
+});
+onEvent("button20", "click", function( ) {
+  setText("text_input1", "What is the Profission of Y S Jagan Before CM");
+  setScreen("screen1");
+});
+onEvent("button17", "click", function( ) {
+  setText("text_input1", "What is the Place of Birth of y s Jagan");
+  setScreen("screen1");
+});
+onEvent("button16", "click", function( ) {
+  setText("text_input1", "what is the date of birth of Jagan Mohan Reddy");
+  setScreen("screen1");
+});
+onEvent("button15", "click", function( ) {
+  setText("text_input1", "where did Jagan Mohan Reddy father died");
+  setScreen("screen1");
+});
+onEvent("button13", "click", function( ) {
+  setText("text_input1", "Jagan Mohan Reddy religion was");
+  setScreen("screen1");
+});
+onEvent("button12", "click", function( ) {
+  setText("text_input1", "what is Height and weight of Jagan Mohan Reddy");
+  setScreen("screen1");
+});
+onEvent("button11", "click", function( ) {
+  setText("text_input1", "what is Jagan Mohan Reddy qualification");
+  setScreen("screen1");
+});
+onEvent("button10", "click", function( ) {
+  setText("text_input1", "what is Jagan Mohan Reddy sister name");
+  setScreen("screen1");
+});
+onEvent("button9", "click", function( ) {
+  setText("text_input1", "what are the names of ys jagan daughters");
+  setScreen("screen1");
+});
+onEvent("button5", "click", function( ) {
+  setText("text_input1", "what is the full name of ysrcp Party");
+  setScreen("screen1");
+});
+onEvent("button5", "click", function( ) {
+  setText("text_input1", "what is the full name of ysrcp Party");
+  setScreen("screen1");
+});
+onEvent("button5", "click", function( ) {
+  setText("text_input1", "what is the full name of ysrcp Party");
+  setScreen("screen1");
+});
+onEvent("button6", "click", function( ) {
+  setText("text_input1", "ys jagan mohan reddy took charge as CM of AP");
+  setScreen("screen1");
+});
+onEvent("button7", "click", function( ) {
+  setText("text_input1", "what is the name of Jagan Mohan Reddy Wife");
+  setScreen("screen1");
+});
+onEvent("button4", "click", function( ) {
+  setText("text_input1", "what is the Full Name of Y S Jagan Mohan Reddy");
+  setScreen("screen1");
+});
+onEvent("text_input1", "click", function( ) {
+  setNumber("text_input1", " ");
+  setText("text_input1", "");
+});
+hideElement("image16");
+hideElement("image17");
+onEvent("button59", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button33", "click", function( ) {
+  hideElement("button33");
+  timedLoop(1000, function() {
+    showElement("button33");
+  });
+  setProperty("text_area2", "border-color", "black");
+  playSound("assets/Jagan-5MB.mp3", false);
+  setProperty("text_area2", "border-width", 2);
+  setProperty("button60", "background-color", "green");
+  setProperty("button50", "background-color", rgb(0,64,225,0));
+  setProperty("button49", "background-color", rgb(0,64,225,0));
+  setProperty("button62", "background-color", rgb(0,64,225,0));
+});
+onEvent("button62", "click", function( ) {
+  setProperty("text_area2", "border-width", 0);
+  stopSound();
+  setProperty("button62", "background-color", "red");
+  setProperty("button60", "background-color", rgb(0,64,225,0));
+  setProperty("button50", "background-color", rgb(0,64,225,0));
+  setProperty("button49", "background-color", rgb(0,64,225,0));
+});
+onEvent("button48", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("button50", "click", function( ) {
+  setProperty("button60", "background-color", rgb(0,64,225,0));
+  setProperty("button62", "background-color", rgb(0,64,225,0));
+  setProperty("button49", "background-color", rgb(0,64,225,0));
+  setProperty("text_area1", "border-color", "black");
+  playSound("assets/hfvhhj.mp3", false);
+  setProperty("text_area1", "border-width", 2);
+  setProperty("button50", "background-color", "green");
+});
+onEvent("button49", "click", function( ) {
+  setProperty("button60", "background-color", rgb(0,64,225,0));
+  setProperty("button62", "background-color", rgb(0,64,225,0));
+  setProperty("button50", "background-color", rgb(0,64,225,0));
+  stopSound();
+  setProperty("text_area1", "border-width", 0);
+  setProperty("button49", "background-color", "red");
+});
